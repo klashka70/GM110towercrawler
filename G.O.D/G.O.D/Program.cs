48799cf12f463803d96c9b34b72959b99d1c2f30
Klash <chris.klashka@snhu.edu>
2025-03-26 10:50:18 -0400

    create a project

---
+++ G.O.D/G.O.D/Program.cs
@@ -0,0 +1,17 @@
+﻿
+// track players health
+// track players armor
+// track players current floor
+// Build out floors for tower
+// Add a method for going up a floor
+// Add a method for going down a floor
+// Add a method for dealing damage to the player
+// Add a method for healing damage to the player
+// Track the enemys current floor
+// Add a method for enemy to go up and down a floor
+// Add a method to track the enemys health
+// Show a game over when the players health drops to zero
+// Show a victory when the player beats the enemy
+// Add a method for dealing damage to the enemy
+
+Console.WriteLine("Enter the Game of Death");
